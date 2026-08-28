f988d60185aaad9e75566136a2ed61713eb0a961
YC-ORZ <138956742+lvzscn@users.noreply.github.com>
2026-08-28 10:35:22 +0800

    更新 libwrt.sh

--- libwrt.sh
+++ libwrt.sh
@@ -107,7 +107,6 @@ keywords_to_delete=(
     #"xiaomi_ax3600" "xiaomi_ax9000" "xiaomi_ax1800" "glinet" "jdcloud_ax6600" "mr7350" 
     "uugamebooster" "luci-app-wol" "luci-i18n-wol-zh-cn" "CONFIG_TARGET_INITRAMFS" "ddns" "LSUSB" "mihomo"
     "smartdns" "kucat" "bootstrap" "uugamebooster" "luci-app-wol" "luci-i18n-wol-zh-cn" "CONFIG_TARGET_INITRAMFS" "ddns" "LSUSB" "mihomo"
-    "smartdns" "kucat" "bootstrap"
     "dockerd" "docker" "containerd" "runc" "tini" "luci-app-dockerman" "luci-i18n-dockerman-zh-cn"
 )
 

635b2232624b92008aab752c041f911c16d270a7
YC-ORZ <138956742+lvzscn@users.noreply.github.com>
2026-08-28 08:47:01 +0800

    更新 libwrt.sh

--- libwrt.sh
+++ libwrt.sh
@@ -87,6 +87,9 @@ wget https://raw.githubusercontent.com/lisaac/luci-app-diskman/master/Parted.Mak
 
 UPDATE_PACKAGE "frp" "https://github.com/ysuolmai/openwrt-frp.git" "master"
 
+# 添加 turboacc 软件包
+curl -sSL https://raw.githubusercontent.com/mufeng05/turboacc/main/add_turboacc.sh -o add_turboacc.sh && bash add_turboacc.sh
+
 # 只保留指定的 qualcommax_ipq60xx 设备
 if [[ $FIRMWARE_TAG == *"EMMC"* ]]; then
     # 有 EMMC 时，只保留：redmi_ax5-jdcloud / jdcloud_re-ss-01 / jdcloud_re-cs-07
@@ -103,7 +106,9 @@ sed -i "/^CONFIG_TARGET_DEVICE_qualcommax_ipq60xx_DEVICE_/{
 keywords_to_delete=(
     #"xiaomi_ax3600" "xiaomi_ax9000" "xiaomi_ax1800" "glinet" "jdcloud_ax6600" "mr7350" 
     "uugamebooster" "luci-app-wol" "luci-i18n-wol-zh-cn" "CONFIG_TARGET_INITRAMFS" "ddns" "LSUSB" "mihomo"
+    "smartdns" "kucat" "bootstrap" "uugamebooster" "luci-app-wol" "luci-i18n-wol-zh-cn" "CONFIG_TARGET_INITRAMFS" "ddns" "LSUSB" "mihomo"
     "smartdns" "kucat" "bootstrap"
+    "dockerd" "docker" "containerd" "runc" "tini" "luci-app-dockerman" "luci-i18n-dockerman-zh-cn"
 )
 
 
@@ -399,4 +404,4 @@ patch_openwrt_go() {
 }
 
 # 执行补丁
-patch_openwrt_go || exit 1
+patch_openwrt_go || echo
